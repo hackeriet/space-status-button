8c2c77e663a2bd17ed1e30e5cb7fba94d3651432
Stig Otnes Kolstad <stigok@broentech.no>
2018-02-20 23:01:58 +0100

    Indent with 4 spaces

--- irc-topic-changer.py
+++ irc-topic-changer.py
@@ -25,33 +25,33 @@ s.connect((HOST, PORT))
 f = s.makefile(mode='r', encoding=ENCODING, newline=EOL)
 
 def sendline(line):
-  return s.send(bytes("%s%s" % (line, EOL), ENCODING))
+    return s.send(bytes("%s%s" % (line, EOL), ENCODING))
 
 def changetopic(status):
-  # Avoid setting an empty topic on start
-  if len(topic) > 0:
-    # The existing topic is expected to contain "space is: <topic> |"
-    new_topic = re.sub(r"(space is: )([^\|]+)", r"\1%s " % status, topic)
-    sendline("TOPIC %s :%s" % (CHANNEL, new_topic))
+    # Avoid setting an empty topic on start
+    if len(topic) > 0:
+        # The existing topic is expected to contain "space is: <topic> |"
+        new_topic = re.sub(r"(space is: )([^\|]+)", r"\1%s " % status, topic)
+        sendline("TOPIC %s :%s" % (CHANNEL, new_topic))
 
 sendline("NICK %s" % NICK)
 sendline("USER %s 0 * :%s" % (NICK, REALNAME))
 sendline("JOIN %s" % CHANNEL)
 
 def journal_reader():
-  proc = Popen(["journalctl", "-f", "-u", "buttond"], stdout=PIPE, bufsize=1)
-  for line in iter(proc.stdout.readline, b''):
-    line = line.decode('utf-8')
-    search = re.search(r"Button state set to (0|1)", line)
-    if search is not None:
-      button_state = search.group(1)
-      if button_state == '1':
-        changetopic("OPEN")
-      else:
-        changetopic("CLOSED")
+    proc = Popen(["journalctl", "-f", "-u", "buttond"], stdout=PIPE, bufsize=1)
+    for line in iter(proc.stdout.readline, b''):
+        line = line.decode('utf-8')
+        search = re.search(r"Button state set to (0|1)", line)
+        if search is not None:
+            button_state = search.group(1)
+            if button_state == '1':
+                changetopic("OPEN")
+            else:
+                changetopic("CLOSED")
 
-    # TODO: logger.verbose
-    print("** " + re.sub(r"[\r\n]+", "", line))
+        # TODO: logger.verbose
+        print("** " + re.sub(r"[\r\n]+", "", line))
 
 # Read the journal on a separate thread to determine if the button
 # has been pressed.
@@ -62,30 +62,29 @@ topic = ''
 
 # Handle IRC connection and its events
 while 1:
-  try:
-    line = f.readline().rstrip()
-  except UnicodeDecodeError as e:
-    print("Failed to decode; ignoring line.", e, file=sys.stderr)
-    continue
+    try:
+        line = f.readline().rstrip()
+    except UnicodeDecodeError as e:
+        print("Failed to decode; ignoring line.", e, file=sys.stderr)
+        continue
 
-  parts = line.split()
+    parts = line.split()
 
-  if parts:
-    # Perform the most important IRC task
-    if parts[0] == "PING":
-      sendline("PONG %s" % line[1])
+    if parts:
+        # Perform the most important IRC task
+        if parts[0] == "PING":
+            sendline("PONG %s" % line[1])
 
-    # Save advertised topic on join
-    if parts[1] == "332":
-      topic = ' '.join(parts[4:])[1:]
-      print("** Initial topic saved to cache: %s" % topic)
+        # Save advertised topic on join
+        if parts[1] == "332":
+            topic = ' '.join(parts[4:])[1:]
+            print("** Initial topic saved to cache: %s" % topic)
 
-    # Save updated updated topics
-    if parts[1] == "TOPIC":
-      topic = ' '.join(parts[3:])[1:]
-      # TODO: logger.<?>
-      print("** New topic saved to cache: %s" % topic)
+        # Save updated updated topics
+        if parts[1] == "TOPIC":
+            topic = ' '.join(parts[3:])[1:]
+            # TODO: logger.<?>
+            print("** New topic saved to cache: %s" % topic)
 
-    # TODO: logger.debug
-    print(line)
-
+        # TODO: logger.debug
+        print(line)
